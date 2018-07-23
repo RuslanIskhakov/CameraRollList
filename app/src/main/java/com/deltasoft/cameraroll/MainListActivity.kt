bb45f20cabf8ea5600556397d6e1acfd59766037
Ruslan Iskhakov <deltasoftmailbox@gmail.com>
2018-07-23 12:52:26 +0500

    Create project from scratch

---
+++ app/src/main/java/com/deltasoft/cameraroll/MainListActivity.kt
@@ -0,0 +1,12 @@
+package com.deltasoft.cameraroll
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_list)
+    }
+}
