c6bfec55d37343fc414b08fbfd3c6032b59428e2
Ruslan Iskhakov <deltasoftmailbox@gmail.com>
2018-07-26 23:33:52 +0500

    Request user for WRITE_EXTERNAL_STORAGE permission

--- app/src/main/java/com/deltasoft/cameraroll/MainListActivity.kt
+++ app/src/main/java/com/deltasoft/cameraroll/MainListActivity.kt
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
 import android.util.Log
+import com.deltasoft.cameraroll.videoencoding.ExtractDecodeEditEncodeMux
 import com.deltasoft.cameraroll.videoencoding.VideoEncodingHelper
 
 class MainListActivity : AppCompatActivity() {
@@ -15,30 +16,30 @@ class MainListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_list)
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
             test()
         } else {
-            requestReadExternalStoragePermission()
+            requestWriteExternalStoragePermission()
         }
 
     }
 
-    private fun requestReadExternalStoragePermission() {
+    private fun requestWriteExternalStoragePermission() {
         val permissions = arrayOfNulls<String>(1)
-        permissions[0] = Manifest.permission.READ_EXTERNAL_STORAGE
+        permissions[0] = Manifest.permission.WRITE_EXTERNAL_STORAGE
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             requestPermissions(permissions, 101)
         }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == 101 && permissions[0] == Manifest.permission.READ_EXTERNAL_STORAGE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (requestCode == 101 && permissions[0] == Manifest.permission.WRITE_EXTERNAL_STORAGE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             test()
         }
     }
 
     private fun test() {
-        val helper = VideoEncodingHelper()
-        helper.start()
+        val test = ExtractDecodeEditEncodeMux()
+        test.test()
     }
 }
