601ad3ac8185a0608e948e13c3c573af39ac4c70
Ruslan Iskhakov <deltasoftmailbox@gmail.com>
2018-07-27 16:47:36 +0500

    Insert initial RecyclerView

--- app/src/main/java/com/deltasoft/cameraroll/MainListActivity.kt
+++ app/src/main/java/com/deltasoft/cameraroll/MainListActivity.kt
@@ -6,18 +6,35 @@ import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
-import android.util.Log
+import android.support.v7.widget.LinearLayoutManager
 import com.deltasoft.cameraroll.videoencoding.ExtractDecodeEditEncodeMux
-import com.deltasoft.cameraroll.videoencoding.VideoEncodingHelper
+import com.deltasoft.cameraroll.videoencoding.adapter.ContentsAdapter
+import com.deltasoft.cameraroll.videoencoding.adapter.ContentsItem
+import kotlinx.android.synthetic.main.activity_main_list.*
 
 class MainListActivity : AppCompatActivity() {
+
+    private var mAdapter: ContentsAdapter? = null
+    private var mItems: ArrayList<ContentsItem> = ArrayList<ContentsItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_list)
 
+        mItems.add(ContentsItem(true, "item 0"))
+        mItems.add(ContentsItem(true, "item 1"))
+        mItems.add(ContentsItem(true, "item 2"))
+        mItems.add(ContentsItem(true, "item 3"))
+        mItems.add(ContentsItem(true, "item 4"))
+        mItems.add(ContentsItem(true, "item 5"))
+        mItems.add(ContentsItem(true, "item 6"))
+        mItems.add(ContentsItem(true, "item 7"))
+
+        setupContents()
+
+
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-            test()
+            //test()
         } else {
             requestWriteExternalStoragePermission()
         }
@@ -34,12 +51,22 @@ class MainListActivity : AppCompatActivity() {
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         if (requestCode == 101 && permissions[0] == Manifest.permission.WRITE_EXTERNAL_STORAGE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            test()
+            //test()
         }
     }
 
     private fun test() {
         val test = ExtractDecodeEditEncodeMux()
         test.test()
+    }
+
+    private fun setupContents() {
+        if (mAdapter == null) {
+            mAdapter = ContentsAdapter(mItems)
+            contentsRecyclerView.setLayoutManager(LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false))
+            contentsRecyclerView.setAdapter(mAdapter)
+        } else {
+            mAdapter?.items = mItems
+        }
     }
 }
---
+++ app/src/main/java/com/deltasoft/cameraroll/videoencoding/adapter/ContentsItem.kt
@@ -0,0 +1,4 @@
+package com.deltasoft.cameraroll.videoencoding.adapter
+
+data class ContentsItem (val isVideo: Boolean, val filePath: String){
+}
